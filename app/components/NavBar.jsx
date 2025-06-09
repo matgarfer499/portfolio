90109b38b5755aa13203a9e9ac120ea954fed71b
matgarfer499 <matigarfer14@gmail.com>
2025-06-09 23:27:52 +0200

    style: make navbar sticky

--- app/components/NavBar.jsx
+++ app/components/NavBar.jsx
@@ -14,8 +14,8 @@ export function NavBar() {
 
     return (
         <>
-            <nav className="backdrop-blur-lg sm:backdrop-blur-0 fixed z-50 w-screen flex justify-between items-center h-[60px] bg-transparent">
-                <h2 className="text-3xl text-white font-bold w-2/12 flex justify-center pl-2 sm:pl-0">
+            <nav className="sticky top-2 flex z-50 w-full bg-transparent sm:top-4">
+                <h2 className="text-3xl text-white font-bold flex justify-center pl-2 sm:pl-0">
                     MG
                     <span className="text-3xl text-orange-500 font-bold flex items-end">
                         .
@@ -57,12 +57,12 @@ export function NavBar() {
                         </svg>
                     )}
                 </button>
-                <div className="hidden sm:flex sm:w-3/4 md:w-1/2 justify-evenly items-center">
-                    <Links/>
+                <div className="hidden sm:flex justify-center gap-10 text-center items-center m-auto border shadow-xl px-10 py-1 rounded-full">
+                    <Links />
                 </div>
             </nav>
             {(
-                <div className={`w-screen fixed sm:hidden z-40 top-0 iphone-blur flex flex-col gap-8 text-xl justify-center items-center menu ${menu ? 'isActive' : ''}`}>
+                <div className={`w-full fixed sm:hidden z-40 top-0 iphone-blur flex flex-col gap-8 text-xl justify-center items-center menu ${menu ? 'isActive' : ''}`}>
                     <Links onClick={toggleMenu} />
                 </div>
             )}
