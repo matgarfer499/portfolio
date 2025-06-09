e3abaf48e701cdcedd3e75563c97fdb832f367bf
matgarfer499 <matigarfer14@gmail.com>
2025-06-09 23:27:24 +0200

    refactor: change about me for experiencie

--- app/components/Links.jsx
+++ app/components/Links.jsx
@@ -14,7 +14,7 @@ export function Links({onClick}) {
                 href="#about-me"
                 className={linkClassName}
             >
-                Sobre mí
+                Experiencia
             </a>
             <a
                 onClick={onClick}
