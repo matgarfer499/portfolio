39699de824722c355262e49a5c1f12802ede68f4
matgarfer499 <matigarfer14@gmail.com>
2025-06-16 22:20:43 +0200

    refactor: update Links component styling and structure for improved readability

--- app/components/Links.jsx
+++ app/components/Links.jsx
@@ -1,43 +1,24 @@
-export function Links({onClick}) {
-    const linkClassName = 'text-white hover:text-orange-500 transition duration-50'
+export function Links({ onClick }) {
+    const linkClassName = `text-slate-200 text-md hover:text-orange-500 transition-colors duration-100 ease-in-out`;
+
     return (
-        <>
-            <a
-                onClick={onClick}
-                href="#home"
-                className={linkClassName}
-            >
+        <div className="flex animate-fade-in-delayed gap-4 justify-center whitespace-nowrap">
+            <a onClick={onClick} href="#home" className={linkClassName}>
                 Inicio
             </a>
-            <a
-                onClick={onClick}
-                href="#about-me"
-                className={linkClassName}
-            >
+            <a onClick={onClick} href="#about-me" className={linkClassName}>
                 Experiencia
             </a>
-            <a
-                onClick={onClick}
-                href="#habilities"
-                className={linkClassName}
-            >
+            <a onClick={onClick} href="#habilities" className={linkClassName}>
                 Habilidades
             </a>
-            <a
-                onClick={onClick}
-                href="#proyects"
-                className={linkClassName}
-            >
+            <a onClick={onClick} href="#proyects" className={linkClassName}>
                 Proyectos
             </a>
-            <a
-                onClick={onClick}
-                href="#contact"
-                className={linkClassName}
-            >
+            <a onClick={onClick} href="#contact" className={linkClassName}>
                 Contacto
             </a>
-        </>
+        </div>
     );
 }
 
